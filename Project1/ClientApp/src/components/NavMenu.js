0db299e455cb65ed3e457f9f329a80b07f66aaa2
gh-charles <blakos244@gmail.com>
2022-11-05 09:51:53 -0500

    put logo in navbar

--- Project1/ClientApp/src/components/NavMenu.js
+++ Project1/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from '../assets/sslogo.png'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -26,7 +27,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" id="navbar-ss" light>
           <Container>
-            <NavbarBrand tag={Link} to="/"><img src="/sslogo.png" width="145" height="20" alt=""/></NavbarBrand>
+            <NavbarBrand tag={Link} to="/"><img src={logo} width="145" height="20" alt=""/></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
