89aa9d94b8bb65ba69c0fd02b508e9246009b5dc
gh-charles <blakos244@gmail.com>
2022-11-05 10:07:17 -0500

    outline navbar links

--- Project1/ClientApp/src/components/NavMenu.js
+++ Project1/ClientApp/src/components/NavMenu.js
@@ -32,13 +32,10 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Login</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">About</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
