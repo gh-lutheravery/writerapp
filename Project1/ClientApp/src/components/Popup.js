841683f4eab00b1e9fd41f3881b1e94e16e671f0
gh-charles <blakos244@gmail.com>
2022-11-23 16:40:21 -0600

    make popup component

---
+++ Project1/ClientApp/src/components/Popup.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+
+export class Popup extends Component {
+    static displayName = Popup.name;
+
+    render() {
+        return (
+            <div>
+                <div id="popup-container">
+
+                </div>
+                <h1>Dashboard</h1>
+                <p>Hello and thank you for using the free tier of writerapp.</p>
+                <a href="#" id="home-link">Click here to report bugs or suggest features.</a>
+                <a href="#" id="home-link">Frequently asked questions.</a>
+                <Roadmap />
+            </div>
+        );
+    }
+}
