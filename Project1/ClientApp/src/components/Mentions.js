ddaa0eeb7427439492011a0c1fafe4797498133b
gh-charles <blakos244@gmail.com>
2022-11-29 23:06:30 -0600

    outline mentions component

---
+++ Project1/ClientApp/src/components/Mentions.js
@@ -0,0 +1,67 @@
+﻿import React, { Component, useState } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { NavMenu } from './NavMenu';
+
+export class Mentions extends Component {
+    static displayName = Layout.name;
+
+    const[dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+    render() {
+        return (
+            <div>
+                <h1 style={{ margin: "auto" }}>*Story* Mentions</h1>
+                <div style={{ display: "flex" }}>
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle} {...props}>
+                        <DropdownToggle caret size="lg">
+                            Sort by
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem>Popularity</DropdownItem>
+                            <DropdownItem>Date</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+
+                    <ListGroup horizontal>
+                        <ListGroupItem
+                            action
+                            active
+                            tag="button"
+                        >
+                            Cras justo odio
+                        </ListGroupItem>
+                        <ListGroupItem
+                            action
+                            tag="button"
+                        >
+                            Dapibus ac facilisis in
+                        </ListGroupItem>
+                        <ListGroupItem
+                            action
+                            tag="button"
+                        >
+                            Morbi leo risus
+                        </ListGroupItem>
+                        <ListGroupItem
+                            action
+                            tag="button"
+                        >
+                            Porta ac consectetur ac
+                        </ListGroupItem>
+                        <ListGroupItem
+                            action
+                            disabled
+                            tag="button"
+                        >
+                            Vestibulum at eros
+                        </ListGroupItem>
+                    </ListGroup>
+                </div>
+
+                <ForeignWebsites/>
+            </div>
+        );
+    }
+}
