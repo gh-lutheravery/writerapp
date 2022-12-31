84bead5851d10b2cb6999567d538484b418ae180
gh-charles <blakos244@gmail.com>
2022-12-30 21:05:12 -0600

    make test responsive

--- src/components/Popularity.js
+++ src/components/Popularity.js
@@ -1,5 +1,5 @@
 import { AreaChart, CartesianGrid, XAxis, 
-        YAxis, Tooltip, Line, Legend, Area } from 'recharts'
+        YAxis, Tooltip, Legend, Area, ResponsiveContainer } from 'recharts'
 
 export function Popularity() {
     // const data = props.reduxDataList
@@ -50,33 +50,35 @@ export function Popularity() {
           amt: 2100
         }
       ];
-      
+
     return (
         <div id="popularity-container">
             <div>
                 <h2>Popularity Graph</h2>
             </div>
             
-            <AreaChart width={730} height={250} data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-                <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                    </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                    </linearGradient>
-                </defs>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
-            </AreaChart>
-
+            <ResponsiveContainer width="100%" height={450}>
+                <AreaChart width={1030} height={450} data={data}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                        </linearGradient>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend />
+                    <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </ResponsiveContainer>
             {/* <AreaChart width={730} height={250} data={data}
             margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
             >
