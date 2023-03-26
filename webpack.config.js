c5c909fe6c7689f38fe1109c2f2590a6b1262659
gh-charles <blakos244@gmail.com>
2023-03-26 14:04:58 -0500

    create dist and correct config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const CopyPlugin = require("copy-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: "src/index.js",
+    module: {
+        rules: [
+            {
+                exclude: "/node_modules/"
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve("manifest.json"), to: path.resolve("dist") },
+                { from: path.resolve("src/assets/square-logo.png"), to: path.resolve("dist") },
+                { from: path.resolve("src/assets/wa-logo.png"), to: path.resolve("dist") }
+                //{ from: "other", to: "public" }
+            ],
+        })
+    ],
+
+    output: {
+        filename: "index.js"
+    }
+}
