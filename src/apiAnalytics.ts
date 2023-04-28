bf258a6441da229e09a1c4a1025b43cefc2ee74c
gh-charles <blakos244@gmail.com>
2023-04-27 22:16:13 -0500

    getPopularityAnalytics pseudocode

--- src/apiAnalytics.ts
+++ src/apiAnalytics.ts
@@ -2,9 +2,17 @@ import { RoyalRoadAPI } from '@mh1024/royalroadl-api/dist/royalroad';
 
 export class Analytics {
     public readonly api: RoyalRoadAPI;
-    constructor(insecure = false) {
-        this.api = new RoyalRoadAPI();
+    constructor(rrApi: RoyalRoadAPI) {
+        this.api = rrApi;
     }
 
-
+    public async getPopularityAnalytics(id: number) {
+        // func that grabs id from url in analyze class
+        // this.api.fiction.getFiction()
+        // func that grabs num from review url in analyze class
+        // func that loops through getreviews
+        // func that sorts review list asc
+        // func that makes dict based on months for each review
+        // put result in obj
+    }
 }
