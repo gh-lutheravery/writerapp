da0ea2630edee90bb21d9d5a32e8a4e301d24681
gh-charles <blakos244@gmail.com>
2023-04-27 22:26:31 -0500

    change to comments

--- src/apiAnalytics.ts
+++ src/apiAnalytics.ts
@@ -10,9 +10,11 @@ export class Analytics {
         // func that grabs id from url in analyze class
         // this.api.fiction.getFiction()
         // func that grabs num from review url in analyze class
-        // func that loops through getreviews
-        // func that sorts review list asc
-        // func that makes dict based on months for each review
+        // func that loops through chapter array and gets real chapter list
+        // func that loops through list and stores comment arrays
+        // func that flattens comment arrays
+        // func that sorts comment list asc
+        // func that makes dict based on months for each comment
         // put result in obj
     }
 }
