6845253033911e0b67458100fc79511adf897e94
gh-charles <blakos244@gmail.com>
2023-04-29 00:24:36 -0500

    create fict obj

--- src/apiAnalytics.ts
+++ src/apiAnalytics.ts
@@ -1,4 +1,5 @@
 import { RoyalRoadAPI } from '@mh1024/royalroadl-api/dist/royalroad';
+import { Fiction } from '@mh1024/royalroadl-api/dist/services/fiction';
 
 export class Analytics {
     public readonly api: RoyalRoadAPI;
@@ -6,9 +7,12 @@ export class Analytics {
         this.api = rrApi;
     }
 
-    public async getPopularityAnalytics(id: number) {
+    public async getPopularityAnalytics(url: string) {
         // func that grabs id from url in analyze class
+        const trimmedUrl: string = url.trim();
+        const id: number = +trimmedUrl.slice(34, 39);
         // this.api.fiction.getFiction()
+        const fict: Fiction = this.api.fiction.getFiction(id);
         // func that grabs num from review url in analyze class
         // func that loops through chapter array and gets real chapter list
         // func that loops through list and stores comment arrays
