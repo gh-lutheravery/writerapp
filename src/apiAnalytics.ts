b1e082fba8131db353e262863fbf4dc6569694c0
gh-charles <blakos244@gmail.com>
2023-05-07 19:57:46 -0500

    finish getpopanalytics

--- src/apiAnalytics.ts
+++ src/apiAnalytics.ts
@@ -1,5 +1,7 @@
 import { RoyalRoadAPI } from '@mh1024/royalroadl-api/dist/royalroad';
 import { Fiction } from '@mh1024/royalroadl-api/dist/services/fiction';
+import { Chapter, ChapterComment } from '@mh1024/royalroadl-api/dist/services/chapter';
+import { getLastPage } from '@mh1024/royalroadl-api/dist/utils';
 
 export class Analytics {
     public readonly api: RoyalRoadAPI;
@@ -15,10 +17,54 @@ export class Analytics {
         const fict: Fiction = this.api.fiction.getFiction(id);
         // func that grabs num from review url in analyze class
         // func that loops through chapter array and gets real chapter list
+        let twoDimComArray: [ChapterComment[]] = [[]];
+        let page: number = 0;
+
+        /*
+        const req = this.api.Requester;
+        const baseUrl = trimmedUrl.slice(0, 24);
+        const htmlPage = await this.req.get(
+            `/fiction/0/_/chapter/${String(ch.id)}/_`,
+        );
+        const lastPage: number = getLastPage(htmlPage);
+        */
+        
         // func that loops through list and stores comment arrays
+        fict.chapters.forEach((ch: Chapter) => {
+            while(true) { 
+                try {
+                    const comArray: ChapterComment[] = this.api.chapter.getComments(ch.id, page);
+                    twoDimComArray.push(comArray)
+                    page++;
+                }
+                catch (error) {
+                    break;
+                }
+            }
+        });
+
         // func that flattens comment arrays
+        const flatComArray: ChapterComment[] = twoDimComArray.flat();
+
         // func that sorts comment list asc
+        const ascComArray: ChapterComment[] = twoDimComArray.sort();
+        
         // func that makes dict based on months for each comment
+        let comDict = new Map();
+        let month: number = 0;
+        for (let com of ascComArray) {
+            const date = new Date(com.release);
+            if (month < date.getMonth()) {
+                comDict[date.getMonth()] = []
+                month = date.getMonth();
+            }
+            else {
+                // add com
+                comDict[month].push(com)
+            }
+        }
+        
         // put result in obj
+        return comDict;
     }
 }
