6f305c470580e528caf2dd1dbbb986a7261f3d35
gh-charles <blakos244@gmail.com>
2023-06-15 00:30:42 -0500

    finish data processing in Popularity

--- src/components/Popularity.js
+++ src/components/Popularity.js
@@ -1,56 +1,18 @@
 import { AreaChart, CartesianGrid, XAxis, 
         YAxis, Tooltip, Legend, Area, ResponsiveContainer } from 'recharts'
+import { getPopularityAnalytics } from '../apiAnalytics'
 
-export function Popularity() {
-    // const data = props.reduxDataList
-    // const data = [{ name: 'a', value: [2, 4, 34, 32, 3, 5, 53, 73, 
-    //     79, 133, 423, 468, 785, 1254, 2311] }]
-
-    const data = [
-        {
-          name: "Page A",
-          uv: 4000,
-          pv: 2400,
-          amt: 2400
-        },
-        {
-          name: "Page B",
-          uv: 3000,
-          pv: 1398,
-          amt: 2210
-        },
-        {
-          name: "Page C",
-          uv: 2000,
-          pv: 9800,
-          amt: 2290
-        },
-        {
-          name: "Page D",
-          uv: 2780,
-          pv: 3908,
-          amt: 2000
-        },
-        {
-          name: "Page E",
-          uv: 1890,
-          pv: 4800,
-          amt: 2181
-        },
-        {
-          name: "Page F",
-          uv: 2390,
-          pv: 3800,
-          amt: 2500
-        },
-        {
-          name: "Page G",
-          uv: 3490,
-          pv: 4300,
-          amt: 2100
-        }
-      ];
-
+export function Popularity(url) { 
+    const popularityMap = getPopularityAnalytics(url);
+    const popularityChartData = []
+    popularityMap.forEach(value, key, map => {
+      object = {
+        name: key,
+        comments: value.length
+      }
+      popularityChartData.push(object);
+    });
+    
     return (
         <div id="popularity-container">
             <div>
@@ -58,7 +20,7 @@ export function Popularity() {
             </div>
             
             <ResponsiveContainer width="100%" height={450}>
-                <AreaChart width={1030} height={450} data={data}
+                <AreaChart width={1030} height={450} data={popularityChartData}
                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
                         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
