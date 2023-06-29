8e4773038195e33a0cb49362f3d1ea0e4b46bdce
gh-charles <blakos244@gmail.com>
2023-06-28 23:52:56 -0500

    implement cards to payment

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Roadmap } from './Roadmap';
+import { Card, CardBody, CardText, CardTitle, Button } from 'reactstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -9,29 +9,49 @@ export class Home extends Component {
       <div>
         <h1>Payment</h1>
 
-        <div>
-          <h2>Free</h2>
-          <p>The free tier of writerapp currently includes:</p>
-          <ul>
-            <li>Check the previous works of an author</li>
-            <li>Analyze the current top 5 genres of Royal Road and see if the selected fiction matches them</li>
-            <li>Observe a detailed graph representing the consistency of the selected fiction</li>
-          </ul>
-        </div>
+        <Card
+          className="my-2"
+          style={{
+            width: '18rem'
+          }}
+        >
+          <CardBody>
+            <CardTitle tag="h5">
+              Free
+            </CardTitle>
+            <CardText>
+              The free tier of writerapp currently includes:
+            </CardText>
+            <ul>
+              <li>Check the previous works of an author</li>
+              <li>Analyze the current top 5 genres of Royal Road and see if the selected fiction matches them</li>
+              <li>Observe a detailed graph representing the consistency of the selected fiction</li>
+            </ul>
+          </CardBody>
+        </Card>
 
-        <div>
-          <h2>Plus</h2>
-          <p>The plus tier of writerapp currently includes:</p>
-          <ul>
-            <li>See a detailed graph of how many readers engaged to a fiction overtime (using comments in each chapter)</li>
-          </ul>
-          <div id="popup-home">
-              <Button onClick={extpay.openPaymentPage()}>
+        <Card
+          className="my-2"
+          style={{
+            width: '18rem'
+          }}
+        >
+          <CardBody>
+            <CardTitle tag="h5">
+              Plus
+            </CardTitle>
+            <CardText>
+              The plus tier of writerapp currently includes:
+            </CardText>
+            <ul>
+              <li>See a detailed graph of how many readers engaged to a fiction overtime (using comments in each chapter)</li>
+            </ul>
+            <Button onClick={extpay.openPaymentPage()}>
                   Pay
-              </Button>
-          </div>
-        </div>
-        
+            </Button>
+          </CardBody>
+        </Card>
+
         <a href="#" id="home-link">Click here to see what tier of Writerapp you currently have.</a>
       </div>
     );
