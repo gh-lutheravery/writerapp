c17bf6eeb888fb40e9d72efb266d0bcabc078c29
gh-charles <blakos244@gmail.com>
2023-06-28 19:22:08 -0500

    export user

--- src/background.js
+++ src/background.js
@@ -4,18 +4,8 @@ const extpay = ExtPay('writerapp-plus');
 
 extpay.startBackground();
 
-let user = extpay.getUser().then(user => {
+let user = await extpay.getUser().then(user => {
     return user
 })
 
-
-chrome.storage.local.get('foo', function() {
-    var extpay = ExtPay('sample-extension');
-    // ...
-})
-
-// use callback to get user object out of promise from getUser() (.then is callback, get user out of then)
-
-export default {
-    USER: user
-}
+export default user;
