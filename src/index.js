da8e9b4ff56f98565958e5591c0a7caa0c80f154
gh-charles <blakos244@gmail.com>
2023-07-01 17:07:25 -0500

    fix imports in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker.js';
 import './bootstrap.css'
-import { Popup } from './Popup';
+import { Popup } from './Popup.js';
 
 const rootElement = document.getElementById('root');
 
